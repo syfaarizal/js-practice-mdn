79807cd2049416cc15788d0fc5b2766f10bdf64e
syfaarizal <syifairgi@gmail.com>
2025-08-12 21:32:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.getElementById("count-el").innerText = 5;
+
+// let count = 0;
+
+// console.log(count);
+
+// let, const & var
+// let myAge = 17;
+
+// console.log(myAge); 
+
+// function test () {
+//     let nama = "Budi";
+//     console.log(nama);
+// }
+
+// test();
+
+function increment() {
+    console.log("The button was clicked");
+}
