5dc361a8c75f9d0c98a9ba73d5c154bd78b215f7
syfaarizal <syifairgi@gmail.com>
2025-08-15 20:28:03 +0700

    innerText

--- script.js
+++ script.js
@@ -16,8 +16,15 @@
 
 // test();
 
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
 function increment() {
-    console.log("The button was clicked");
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
 }
 
 // let count = 0;
